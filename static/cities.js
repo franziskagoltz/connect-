91cbcd0663a2c6cda7f0ea5a908552d8b65badd5
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-03-12 22:22:00 -0700

    wrote ajax call to get connections filtered by city and successfunction that updates DOM with matching connections

--- static/cities.js
+++ static/cities.js
@@ -1,10 +1,30 @@
 "use strict";
 
 
+function filter_cities(data) {
+
+    // instantiating an array of contacts living in the selected city
+    var contacts = [];
+
+// looping over json result
+    for (var i = 0; i < data[0].length; i++) {
+
+        // adding first name of contact to contacts array with html elements
+        contacts.push('<li>'+data[0][i].first_name+'</li');
+        
+    };
+
+    // updating the DOM with the filtered contacts
+    $("#filterd").html(contacts);
+}
+
+
 $('.city').on('click', function() {
 
     var city = $(this).val();
 
     console.log(city);
+
+    $.post("/cities.json", {"city": city}, filter_cities)
 
 });
